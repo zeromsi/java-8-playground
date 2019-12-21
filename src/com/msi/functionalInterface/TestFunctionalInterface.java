afa8e530c28ffd01c2dff01c68b980ad8d787fe6
zeromsi <www.zeromsi2@gmail.com>
2019-12-21 22:54:54 +0600

    fixes#3 , fixes#4

--- src/com/msi/functionalInterface/TestFunctionalInterface.java
+++ src/com/msi/functionalInterface/TestFunctionalInterface.java
@@ -1,5 +1,13 @@
 package com.msi.functionalInterface;
 
 public interface TestFunctionalInterface {
-
+	public abstract void printSomething();
+	
+	public default void printHelloWorld() {
+		System.out.println("Hello World");
+	}
+	
+	public default void printHi() {
+		System.out.println("Hi");
+	}
 }
